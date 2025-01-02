ee77cb07fa34f6407b7d40bf7cf354f0ebeb4ba6
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-02 14:15:30 +0500

    Added Post Data

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func GetData(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "Hello world",
+	})
+}
+
+func PostData(context *gin.Context) {
+	body := context.Request.Body
+	context.IndentedJSON(http.StatusOK, body)
+
+	content, err := io.ReadAll(body)
+	if err != nil {
+		fmt.Println("error reading body")
+	}
+
+	context.JSON(200, gin.H{
+		"bodyData": string(content),
+	})
+
+}
 func main() {
 
 	// Default logger and middleware
 	router := gin.Default()
 
-	router.GET("/getdata", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "Hello world",
-		})
-	})
+	router.GET("/getdata", GetData)
+	router.POST("/postdata", PostData)
 
 	router.Run()
 }
